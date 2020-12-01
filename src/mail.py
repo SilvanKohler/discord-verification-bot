4c66f86682127e43bc25cbc5bae058a9b66f15cb
Silvan Kohler <edu.silvan.kohler@gmail.com>
2020-12-01 21:02:48 +0100

    made things

---
+++ src/mail.py
@@ -0,0 +1,13 @@
+import smtplib
+from os import getenv
+
+port = 587
+server = 'saturn.kreativmedia.ch'
+password = getenv('MAIL_PASSWORD')
+login = getenv('MAIL_LOGIN')
+
+def send(address):
+    with smtplib.SMTP(server, port) as s:
+        s.login(login, password)
+        s.sendmail(login, address, 'You real?')
+        s.close()
